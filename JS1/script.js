d6b8bea252fa7d0aa3800d7ccdc0e1b15538cf97
ilsuley <107783553+ilsuley@users.noreply.github.com>
2022-08-18 07:51:51 +0400

    Update script.js

--- JS1/script.js
+++ JS1/script.js
@@ -1,4 +1,21 @@
-let cavab = confirm("Biz Frontendi ne vat oyrenecik?");
+/* let cavab = confirm("Biz Frontendi ne vaxt oyrenecik?");
 
-document.getElementById("Screen").innerHTML = cavab;
+document.getElementById("Screen").innerHTML = cavab; */
+
+/* let x=10;
+   if(x == 9){
+        alert("duzdu");
+   }
+   else if(x == 10){
+        alert("duzdu")
+   }
+   else{
+        alert("sehfdi");
+   }
+ */
+
+
+   let x = 10;
+
+   x == 8 ? alert("x10") : alert("sehfdi"); x == 9 ? alert("x9") : "sehfdi"
 
