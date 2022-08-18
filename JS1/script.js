bbdb584f7ee038b9bc434949645e7a9ee54e80e0
ilsuley <107783553+ilsuley@users.noreply.github.com>
2022-08-18 12:45:20 +0400

    Update script.js

--- JS1/script.js
+++ JS1/script.js
@@ -1,8 +1,8 @@
-/* let cavab = confirm("Biz Frontendi ne vaxt oyrenecik?");
+let cavab = confirm("Biz Frontendi ne vaxt oyrenecik?");
 
-document.getElementById("Screen").innerHTML = cavab; */
+document.getElementById("Screen").innerHTML = cavab; 
 
-/* let x=10;
+ let x=10;
    if(x == 9){
         alert("duzdu");
    }
@@ -12,10 +12,39 @@ document.getElementById("Screen").innerHTML = cavab; */
    else{
         alert("sehfdi");
    }
- */
+ 
 
 
-   let x = 10;
+    /*  let x = 10; */
 
-   x == 8 ? alert("x10") : alert("sehfdi"); x == 9 ? alert("x9") : "sehfdi"
+   x == 8 ? alert("x10") : alert("sehfdi"); x == 9 ? alert("x9") : "sehfdi" 
 
+
+
+    function Check(){
+     let email = "ilham.suleymanli@gmail.com";
+     let password = 123456789 ;
+
+
+
+     let useremail = document.getElementById("email");
+     let userpassword = document.getElementById("pass");
+
+     console.log(useremail.value)
+     console.log(userpassword.value)
+
+     if(useremail.value == email && userpassword.value == password )
+     alert("duzdu")
+     else{
+     alert('sehfdi')     
+     }
+   }
+ 
+
+   function Check(){
+     let username = "ilsuley";
+
+     let durdane = document.getElementById("username")
+
+     console.log(durdane.value)
+   }
